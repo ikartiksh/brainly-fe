d5d2b433d74c1a956a260cdc9f75f5ab3c266284
ikartiksh <imailkartiksharma@gmail.com>
2025-10-24 23:26:45 +0530

    add button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,8 @@
+import { ReactElement } from "react";
 
+type Variants = "primary" | "secondary"
 export interface ButtonProps {
-    variant: "primary" | "secondary";
+    variant: Variants;
     size: "sm" | "md" | "lg";
     text: string;
     startIcon?: any;
@@ -8,8 +10,22 @@ export interface ButtonProps {
     onClick: () => void;
 }
 
-export const Button = (props: ButtonProps) => {
-    return <button></button>
+const variantStyles = {
+    "primary": "bg-blue-600 text-white",
+    "secondary": "bg-blue-400 text-white"
 }
 
-<Button variant="primary" size="md" onClick={} text={} startIcon={} />
+const sizeStyles = {
+    "sm": "p-2",
+    "md": "p-4",
+    "lg": "p-6"
+}
+
+const defaultStyles = "rounded-md flex"
+
+export const Button = (props: ButtonProps) => {
+    return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>
+        {props.startIcon} {props.text} {props.endIcon}</button>
+}
+
+<Button variant="primary" size="md" onClick={() => {}} text={"asd"} />
