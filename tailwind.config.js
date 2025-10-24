f6390db5fc0e8e0bf5d16841755ee53b3a871feb
ikartiksh <imailkartiksharma@gmail.com>
2025-10-24 18:16:04 +0530

    add frontend assets
    Signed-off-by: ikartiksh <imailkartiksharma@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          600: "#103560",
+          500: "#07609d",
+          300: "#67cade",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
